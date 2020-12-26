69373b5818ebf583bc51bbd8fa074b790ab545a6
= <=>
2020-12-26 19:23:25 +0100

    Spring Data example with postgresql and docker

---
+++ src/main/java/com/refactorizando/example/postgresql/SpringDataPostgresqlExampleApplication.java
@@ -0,0 +1,13 @@
+package com.refactorizando.example.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataPostgresqlExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataPostgresqlExampleApplication.class, args);
+	}
+
+}
